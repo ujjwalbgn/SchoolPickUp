72daea10ae76f46e953948df39b998f60f695652
Ujjwal Bajagain <ubajagain@gmail.com>
2021-05-01 18:49:03 -0500

    API App deleted

--- API_Services/apps.py
+++
@@ -1,6 +0,0 @@
-from django.apps import AppConfig
-
-
-class ApiServicesConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'API_Services'
--- SchoolPickup_backend/settings.py
+++ SchoolPickup_backend/settings.py
@@ -36,7 +36,6 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
 
-    'API_Services.apps.ApiServicesConfig',
     'users.apps.UsersConfig',
 
     'rest_framework',
