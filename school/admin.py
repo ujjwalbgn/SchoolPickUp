fe5b706eb76eb3165f562552bb479741dc3aaeab
aviaryal <58341812+aviaryal@users.noreply.github.com>
2021-06-20 16:34:49 -0500

    schedualrAdded

--- school/admin.py
+++ school/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from school.models import Student, Guardian, Relation,StudentAndGuardian, SchoolDetails, GuardiansLocation
+from school.models import Student, Guardian, Relation,StudentAndGuardian, SchoolDetails, GuardiansLocation,NearestParents
 
 # Register your models here.
 
@@ -9,3 +9,4 @@ admin.site.register(Relation)
 admin.site.register(StudentAndGuardian)
 admin.site.register(SchoolDetails)
 admin.site.register(GuardiansLocation)
+admin.site.register(NearestParents)
